658d03a83228f7e9be680e78d14376803959cd5a
macribo <roibeard.mac.unfraidh@gmail.com>
2017-03-17 12:56:45 +0100

    Separate story from narrative

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-bundle.js: frontfeder.js county-names.js narrative.js badge-selector.js
+bundle.js: frontfeder.js county-names.js narrative.js badge-selector.js story-texts.js
 	node_modules/.bin/browserify frontfeder.js > bundle.js  
--- frontfeder.js
+++ frontfeder.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
+let storyTexts = require("./story-texts");
 let narrative = require("./narrative");
 let badgeSelector = require("./badge-selector");
-badgeSelector();
-narrative();
+badgeSelector(100);
+narrative(storyTexts);
